fede8879f4bbcc0ecce956781cdebba436c75b11
Ash <monsterved2011@outlook.com>
2026-03-23 23:25:34 +0530

    Discount Debugged

--- calculateArea/Functions(Practise).js
+++ calculateArea/Functions(Practise).js
@@ -1,15 +1,16 @@
 let Item1;
 let Item2;
 let Item3;
+let Discount;
 
 function CalculateTotal() {
     Item1 = parseFloat(document.getElementById('Item1').value);
     Item2 = parseFloat(document.getElementById('Item2').value);
     Item3 = parseFloat(document.getElementById('Item3').value);
     Discount = parseFloat(document.getElementById('Discount').value);
-    let ItemT = Item1+Item2+Item3
+    let ItemT = Item1+Item2+Item3;
 
-    let DiscountT = (Item1+Item2+Item3/100)*Discount
-    let Total = ItemT-DiscountT
-    document.getElementById('result').innerText = ("Your total is: ", Total)
+    let discountAmount = (Item1+Item2+Item3)*(Discount/100);
+    let Total = ItemT-discountAmount;
+    document.getElementById('result').innerText = (`Your total is:  ${Total}`);
 }
